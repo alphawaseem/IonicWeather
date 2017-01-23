ca79bb69062920815f2bb2149f7094e56fabbc9c
Waseem Ahmed <alphawaseem@gmail.com>
2017-01-23 20:07:23 +0530

    get hourly forecasts

---
+++ src/model/myweather.model.ts
@@ -0,0 +1,10 @@
+export interface MyWeatherInfo {
+    temp_f:string,
+    temp_c:string,
+    feelslike_f : string,
+    feelslike_c : string,
+    condition : string,
+    icon_url : string,
+    time_string : string,
+    humidity : string
+}
